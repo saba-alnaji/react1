d575247b09c66a210ea8737dbd6fc7c3656104ff
saba-alnaji <sabanaji930@gmail.com>
2026-02-13 00:52:46 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from "./header/Header";
+import Hero from "./hero/Hero";
+import Portfolio from "./portfolio/Portfolio";
+import About from "./about/About";
+import ContactMe from "./contactMe/ContactMe";
+import Footer from "./footer/Footer";
+
+
+
+export default function App() {
+  return <>
+    <Header />
+    <Hero />
+    <Portfolio />
+    <About />
+    <ContactMe />
+    <Footer />
+
+  </>
+}
